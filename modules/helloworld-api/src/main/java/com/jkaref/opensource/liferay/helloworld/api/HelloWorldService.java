cd88297e40256f631ab5a48ba8bd20e20500ad77
Matthias Muenzner <matthias.muenzner@jkaref.com>
2018-02-23 08:58:34 +0100

    * Added second example service

--- modules/helloworld-api/src/main/java/com/jkaref/opensource/liferay/helloworld/api/HelloWorldService.java
+++ modules/helloworld-api/src/main/java/com/jkaref/opensource/liferay/helloworld/api/HelloWorldService.java
@@ -2,7 +2,7 @@
  * Copyright (c) [j]karef GmbH year .
  */
 
-package com.jkaref.opensource.liferay.helloworld.contract;
+package com.jkaref.opensource.liferay.helloworld.api;
 
 /**
  * @author Matthias Muenzner <matthias.muenzner@jkaref.com>
